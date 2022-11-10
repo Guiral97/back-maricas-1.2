566ab8d803e98faa788f5a1365ba7575bf6c1976
aldana <aldanamagali25@gmail.com>
2022-11-10 18:49:12 -0300

    MT-B2-15

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema(
+    {
+      name: { type: String, required: true },
+      lastName: { type: String, required: true },
+      role: { type: String, required: true },
+      photo: { type: String, required: true },
+      age: { type: Number, required: true },
+      email: { type: String, required: true },
+      password: { type: String, required: true },
+      code: { type: String, required: true },
+      verified: { type: Boolean, required: true },
+      logged: { type: Boolean, required: true }
+  }
+);
+
+const User = mongoose.model('admins', schema);
+module.exports = User;
