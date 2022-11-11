402f1f196f98e3a5c59b8ba22789ddfc643addcd
Andres Martinez <guiiral97@gmail.com>
2022-11-11 16:52:05 -0500

    MT-01-21

--- routes/city.js
+++ routes/city.js
@@ -1,9 +1,10 @@
 let router = require('express').Router();
-let { create, read} = require('../controllers/city');
+let { create, read, readOne} = require('../controllers/city');
 
 
 router.post('/', create);
 router.get('/', read);
+router.get("/:id",readOne)
 
 
 module.exports = router;
