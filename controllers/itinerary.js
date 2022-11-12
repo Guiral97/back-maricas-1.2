d5b94b31787d6e994a4c2af15e94f7a19dfcd1d8
Andres Martinez <guiiral97@gmail.com>
2022-11-12 18:41:28 -0500

    MT-B1-33

--- controllers/itinerary.js
+++ controllers/itinerary.js
@@ -27,7 +27,6 @@ const controller = {
                 cityId: req.query.cityId
             };
         }
-
         try {
             let itineraries = await Itinerary.find(query);
             if (itineraries) {
@@ -36,6 +35,30 @@ const controller = {
                     message: 'Itinerary find successfully',
                     data: itineraries,
                 })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: error.message,
+                });
+            }
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    },
+    update: async (req, res) => {
+        let id = req.params.id;
+        try {
+            let itinerary = await Itinerary.findOneAndUpdate({ _id: id }, req.body, {new: true} )
+            
+            if (itinerary) {
+                res.status(200).json({
+                    response: itinerary.id,
+                    success: true,
+                    message: "Itinerary update successfully",
+                });
             } else {
                 res.status(404).json({
                     success: false,
