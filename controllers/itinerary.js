ff07a02608c67025d7c20fa38fc714d7bac29232
Andres Martinez <guiiral97@gmail.com>
2022-11-12 18:17:40 -0500

    MT-B1-31

--- controllers/itinerary.js
+++ controllers/itinerary.js
@@ -3,6 +3,22 @@ const Itinerary = require('../models/Itinerary');
 
 const controller = {
 
+    create: async (req, res) => {
+        try {
+            let itinerary = await Itinerary.create(req.body);
+            res.status(201).json({
+                response: itinerary._id,
+                success: true,
+                message: "Itinerary created successfully",
+            });
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    },
+
     read: async (req, res) => {
 
         let query = {};
--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,8 +1,9 @@
 let router = require('express').Router();
-let { read } = require('../controllers/itinerary');
+let { read, create } = require('../controllers/itinerary');
 
 
 router.get('/', read);
+router.post('/', create);
 
 
 module.exports = router;
