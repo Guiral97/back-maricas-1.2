0e6740ae85faf2791bf1c9c19cba7c0907d10b31
aldana <aldanamagali25@gmail.com>
2022-11-11 21:32:38 -0300

    MT-02-22

--- config/database.js
+++ config/database.js
@@ -1,19 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 let connection = async () => {
-    try {
-        mongoose.connect(
-            
-                process.env.LINK_DB, 
-            {
-                useUnifiedTopology: true,
-                useNewUrlParser: true,
-            }
-        );
-        console.log('Connected to database');
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    mongoose.connect(process.env.LINK_DB, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("Connected to database");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-connection()
+connection();
