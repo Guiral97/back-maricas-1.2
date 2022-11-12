577f9d4b904f3e4784c8adc7beabc687ad6000ff
aldana <aldanamagali25@gmail.com>
2022-11-12 15:18:08 -0300

    MT-02-23

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ let router = require('express').Router();
 let user = require('./user');
 let city = require('./city');
 let itinerary = require('./itinerary');
+let hotel = require('./hotel')
+
 
 
 router.use('/api/users', user);
 router.use('/api/cities', city);
 router.use('/api/itineraries', itinerary);
+router.use('/api/hotels', hotel)
 
 
 module.exports = router;
