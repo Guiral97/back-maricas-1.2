42228849e2e16f3e6ecaceb0748c21745779ba97
aldana <aldanamagali25@gmail.com>
2022-11-12 20:05:00 -0300

    MT-B2-30

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,11 +1,12 @@
 let router = require('express').Router()
-let { create, read, findOne, update } = require("../controllers/hotel")
+let { create, read, findOne, update, destroy} = require("../controllers/hotel")
 
 
 router.post("/", create)
 router.get("/", read)
 router.get("/:id", findOne)
 router.patch("/:id", update)
+router.delete("/:id", destroy)
 
 
 module.exports = router;
