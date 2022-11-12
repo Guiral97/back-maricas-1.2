fdd471dfd024e5b60277f25bcbf24fcf62a66a6d
Andres Martinez <guiiral97@gmail.com>
2022-11-12 15:25:49 -0500

    MT-B1-27

--- controllers/city.js
+++ controllers/city.js
@@ -69,6 +69,30 @@ const controller = {
             });
         }
     },
+    update: async (req, res) => {
+        let id = req.params.id;
+        try {
+            let city = await City.findOneAndUpdate({ _id: id }, req.body, {new: true} )
+            
+            if (city) {
+                res.status(200).json({
+                    response: city,
+                    success: true,
+                    message: "City update successfully",
+                });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: error.message,
+                });
+            }
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    },
 };
 
 module.exports = controller;
--- routes/city.js
+++ routes/city.js
@@ -1,10 +1,11 @@
 let router = require('express').Router();
-let { create, read, findOne} = require('../controllers/city');
+let { create, read, findOne, update} = require('../controllers/city');
 
 
 router.post('/', create);
 router.get('/', read);
 router.get("/:id",findOne)
+router.put("/:id",update)
 
 
 module.exports = router;
