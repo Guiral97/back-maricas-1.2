88cfb8c79c19ba6989105f5360748b34891250fa
Andres Martinez <guiiral97@gmail.com>
2022-11-12 19:16:53 -0500

    MT-B1-35

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,10 +1,11 @@
 let router = require('express').Router();
-let {create, read, update  } = require('../controllers/itinerary');
+let {create, read, update, destroy  } = require('../controllers/itinerary');
 
 
 router.get('/', read);
 router.post('/', create);
 router.put('/:id', update);
+router.delete('/:id', destroy);
 
 
 module.exports = router;
